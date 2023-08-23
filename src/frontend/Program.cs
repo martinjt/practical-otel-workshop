83e9b870a4fabe1e126a02a9e8b9d681f1ecdaaf
MartinDotNet <github@my2cents.co.uk>
2023-08-23 15:07:58 +0100

    Various updates

--- src/frontend/Program.cs
+++ src/frontend/Program.cs
@@ -6,17 +6,27 @@ builder.Services.AddHttpClient();
 builder.Services.AddOpenTelemetry()
     .WithTracing(tpb => 
         tpb.ConfigureResource(resource => resource.AddService("frontend"))
+           .AddSource("myapp")
            .AddAspNetCoreInstrumentation()
            .AddHttpClientInstrumentation()
            .AddConsoleExporter()
            .AddOtlpExporter());
 
+builder.Services.AddSingleton(sp => {
+    return sp.GetRequiredService<TracerProvider>().GetTracer("myapp");
+});
+
 builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
-app.MapGet("/", async (HttpClient httpClient, IConfiguration configuration,
+app.MapGet("/", async (HttpClient httpClient, IConfiguration configuration, Tracer tracer,
     string firstname, string surname) => {
+        
+        // var span = tracer.StartActiveSpan("my internal span");
+
+        // Tracer.CurrentSpan.SetAttribute("firstname", firstname);
+        // return $"Hello {firstname}";
         var backendHostname = configuration["BACKEND_HOSTNAME"];
         var result = await httpClient.GetAsync($"http://{backendHostname}/profile?firstname={firstname}&surname={surname}");
         var response = await result.Content.ReadFromJsonAsync<BackendResponse>();
